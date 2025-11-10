7fbccd97e92c4f18c72c3fc0073758a5f92125f7
bku <>
2025-11-10 21:52:02 +0100

    transfer footer from pages to components

--- Frontend/esportscms/src/app/pages/navbar/navbar.spec.ts
+++ Frontend/esportscms/src/app/pages/navbar/navbar.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Navbar } from './navbar';
+import { Navbar } from '../../components/navbar/navbar';
 
 describe('Navbar', () => {
   let component: Navbar;
