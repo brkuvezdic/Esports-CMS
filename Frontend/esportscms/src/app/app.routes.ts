08a1d2b478207a165a84f8f160870a508fb5f1ff
bku <>
2025-11-13 17:22:37 +0100

    Added route to About and default route on wrong link

--- Frontend/esportscms/src/app/app.routes.ts
+++ Frontend/esportscms/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about';
 
-export const routes: Routes = [ 
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'About', component: AboutComponent },
+  { path: '**', redirectTo: 'home' },
+
 ];
