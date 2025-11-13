28fcc0e20ca60b0241a9eb57b511bcb22cd443ee
bku <>
2025-11-13 19:33:46 +0100

    Added beggining for getColleges API

---
+++ EsportsCmsInfrastructure/CollegeRepository.cs
@@ -0,0 +1,25 @@
+﻿using EsportsCmsApplication.Interfaces.Colleges;
+using EsportsCmsDomain.EntitiesNew;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsportsCmsInfrastructure
+{
+    public class CollegeRepository : ICollegeRepository
+    {
+        private readonly EsportsCmsContext _dbContext;
+
+        public CollegeRepository(EsportsCmsContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public  Task<List<College>> GetAllCollegesAsync()
+        {
+            return  _dbContext.Colleges.ToListAsync();
+        }
+    }
+}
